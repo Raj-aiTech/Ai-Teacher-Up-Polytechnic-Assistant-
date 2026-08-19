fc1222c168208fa5e5f3718f8f672f4c9d49cfd9
Raj-aiTech <mkmaxworld@gmail.com>
2026-08-19 22:41:18 +0530

    Create CSE and IT subject structure

---
+++ subjects.js
@@ -0,0 +1,25 @@
+const subjects = {
+  "Computer Science & Engineering": {
+    code: "355",
+    semesters: {
+      "Semester 1": [],
+      "Semester 2": [],
+      "Semester 3": [],
+      "Semester 4": [],
+      "Semester 5": [],
+      "Semester 6": []
+    }
+  },
+
+  "Information Technology": {
+    code: "356",
+    semesters: {
+      "Semester 1": [],
+      "Semester 2": [],
+      "Semester 3": [],
+      "Semester 4": [],
+      "Semester 5": [],
+      "Semester 6": []
+    }
+  }
+};
